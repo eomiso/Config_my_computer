b3af47d2e338090b31191c9384617e2214487f1d
eomiso <ytrtef@gmail.com>
2020-07-30 19:46:39 +0900

    Create install_oh_my_zsh.sh

---
+++ install_oh_my_zsh.sh
@@ -0,0 +1,9 @@
+if command -v curl &> /dev/null; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+elif command -v wget &> /dev/null; then
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo -en "You need curl or wget. "
+  echo "Unexpected termination of $0"
+  exit 0
+fi
